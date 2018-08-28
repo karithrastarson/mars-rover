9f10fd668b2efa7cdbd0cdae8134ebb99f3276bf
Kári Thrastarson <kath@netcompany.com>
2018-08-28 20:50:36 +0200

    The Manouevre enum removed as it was not used in the implementation

--- marsrover/src/com/mars/tools/Manoeuvre.java
+++
@@ -1,9 +0,0 @@
-package com.mars.tools;
-/*This enum class are the possible manoeuvres that the rover can make:
- * L for turning left
- * R for turning right
- * M for moving forward
- */ 
-public enum Manoeuvre {
-L, R, M;
-}
